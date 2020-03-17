b1950a47a30bfe0ec7a78ab0fc9741c18fc00456
叶俊 <2512636001@qq.com>
2020-03-17 08:28:00 +0800

    my first init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// 消息提示的环境配置，设置为开发环境或者生产环境
 Vue.config.productionTip = false;
 
 new Vue({
