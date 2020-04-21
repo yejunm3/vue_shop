388acd7db69289c418b305fa2fe0b68173203bfd
叶俊 <2512636001@qq.com>
2020-04-21 14:31:41 +0800

    完成订单列表组件

--- src/components/order/Orders.vue
+++ src/components/order/Orders.vue
@@ -27,16 +27,16 @@
         </el-table-column>
         <el-table-column label="支付状态">
           <template slot-scope="scope">
-            <el-tag type="danger" v-if="scope.row.pay_status === '0'">未支付</el-tag>
-            <el-tag type="success" v-else-if="scope.row.pay_status === '1'">已支付</el-tag>
+            <el-tag type="danger" v-if="scope.row.order_pay === '0'">未支付</el-tag>
+            <el-tag type="success" v-else>已支付</el-tag>
           </template>
         </el-table-column>
         <el-table-column label="支付方式">
           <template slot-scope="scope">
             <el-tag type="danger" v-if="scope.row.order_pay === '0'">未支付</el-tag>
-            <el-tag type="primary" v-else-if="scope.row.order_pay === '1'">支付宝</el-tag>
-            <el-tag type="primary" v-else-if="scope.row.order_pay === '2'">微信</el-tag>
-            <el-tag type="primary" v-else-if="scope.row.order_pay === '3'">银行卡</el-tag>
+            <el-tag type="success" v-else-if="scope.row.order_pay === '1'">支付宝</el-tag>
+            <el-tag type="success" v-else-if="scope.row.order_pay === '2'">微信</el-tag>
+            <el-tag type="success" v-else-if="scope.row.order_pay === '3'">银行卡</el-tag>
           </template>
         </el-table-column>
         <el-table-column label="创建时间" width="180">
@@ -53,7 +53,7 @@
           <template slot-scope="scope">
             <el-button type="primary" size="small" icon="el-icon-truck" @click="showDeliveryDetails">物流</el-button>
             <el-button type="primary" size="small" icon="el-icon-view" @click="showOrdersDetails(scope.row.order_id)">详情</el-button>
-            <el-button type="primary" size="small" icon="el-icon-edit" @click="showOrdersDetails(scope.row.order_id)">地址</el-button>
+            <el-button type="primary" size="small" icon="el-icon-map-location" @click="showAddressDetails(scope.row.order_id)">地址</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -90,19 +90,19 @@
         <el-row>
           <el-col :span="6">是否发货</el-col>
           <el-col :span="18">
-            <el-switch v-model="editOrdersDetails.is_send" active-text="已发货" inactive-text="未发货"></el-switch>
+            <el-switch v-model="editOrdersDetails.is_send" @change="handleSendChange" active-text="已发货" inactive-text="未发货"></el-switch>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="6">支付状态</el-col>
           <el-col :span="18">
-            <el-switch v-model="editOrdersDetails.pay_status" active-text="已付款" inactive-text="已付款"></el-switch>
+            <el-switch v-model="editOrdersDetails.pay_status" active-text="已付款" inactive-text="未付款" disabled></el-switch>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="6">支付方式</el-col>
           <el-col :span="18">
-            <el-radio-group v-model="editOrdersDetails.order_pay" size="small">
+            <el-radio-group v-model="editOrdersDetails.order_pay" @change="handleRadioChange" size="small">
               <el-radio-button :label="'0'">未支付</el-radio-button>
               <el-radio-button :label="'1'">支付宝</el-radio-button>
               <el-radio-button :label="'2'">微信</el-radio-button>
@@ -121,14 +121,33 @@
       </div>
     </el-drawer>
 
+    <!-- 更改地址 -->
+    <el-drawer :with-header="false" :visible.sync="addressDetails" @closed="cancleAddressForm">
+      <el-alert title="更改地址" type="info" show-icon center></el-alert>
+      <el-form ref="addressForm" label-width="80px">
+        <el-form-item label="详细地址">
+          <el-input v-model="addressValue1"></el-input>
+        </el-form-item>
+        <el-form-item label="省市联动">
+          <el-cascader class="address-react" v-model="addressValue2" :options="regionData"></el-cascader>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitAddressForm">确认修改</el-button>
+          <el-button @click="cancleAddressForm">取消修改</el-button>
+        </el-form-item>
+      </el-form>
+    </el-drawer>
+
   </div>
 </template>
 
 <script>
 import _ from 'lodash'
+import { regionData, CodeToText } from 'element-china-area-data'
 export default {
   data() {
     return {
+      // 获取列表
       ordersQuery: {
         query: '',
         pagenum: 1,
@@ -141,8 +160,6 @@ export default {
       deliveryDetailsList: [],
       // 订单详情
       ordersDetails: false,
-      ordersDetailsList: [],
-      // 修改订单
       editOrdersId: '',
       editOrdersDetails: {
         is_send: '',
@@ -150,10 +167,18 @@ export default {
         order_price: '',
         order_number: '',
         pay_status: ''
-      }
+      },
+      editOrdersChange: false,
+      // 修改地址
+      addressDetails: false,
+      addressValue1: '',
+      addressValue2: '',
+      // 联动数据
+      regionData: regionData
     }
   },
   methods: {
+    // 获取列表
     async getOrdersList() {
       const { data: res } = await this.$axios.get('orders', { params: this.ordersQuery })
       console.log(res)
@@ -186,9 +211,9 @@ export default {
       // 
       this.editOrdersId = res.data.order_id
       this.editOrdersDetails.is_send = (res.data.is_send === '否' ? false : true)
+      this.editOrdersDetails.pay_status = (res.data.order_pay === '0' ? false : true)
       this.editOrdersDetails.order_pay = res.data.order_pay
       this.editOrdersDetails.order_price = res.data.order_price
-      this.editOrdersDetails.pay_status = (res.data.pay_status == 0 ? false : true)
       this.editOrdersDetails.order_number = this.getOrderNumber(res.data.goods)
     },
     // 获取商品数量
@@ -201,14 +226,49 @@ export default {
     },
     // 订单详情关闭
     async handleClose() {
+      if (!this.editOrdersChange) return
       let ordersListClone = _.cloneDeep(this.editOrdersDetails)
       // 
       const { data:res } = await this.$axios.put('orders/' + this.editOrdersId, ordersListClone)
       console.log(res)
-      if (res.meta.status !== 201) return this.$message.error('更新失败')
+      if (res.meta. status!== 201) return this.$message.error('更新失败')
       this.$message.success('更新成功')
+      this.editOrdersChange = false
       this.getOrdersList()
+    },
+    // 更改发货状态
+    handleSendChange() {
+      this.editOrdersChange = true
+    },
+    // 更改支付方式
+    handleRadioChange(value) {
+      if (value === '0') {
+        this.editOrdersDetails.pay_status = false
+      } else {
+        this.editOrdersDetails.pay_status = true
+      }
+      this.editOrdersChange = true
+    },
+    // 修改地址展示
+    showAddressDetails() {
+      this.addressDetails = true
+    },
+    submitAddressForm() {
+      if (this.addressValue1 === '' || this.addressValue2 === '') return this.$message.error('请填写完整的地址')
+      this.$message({
+        dangerouslyUseHTMLString: true,
+        message: `
+        <p style="margin-bottom: 5px;">详细地址: ${this.addressValue1}</p>
+        <p>级联选择: ${CodeToText[this.addressValue2[0]]},${CodeToText[this.addressValue2[1]]},${CodeToText[this.addressValue2[2]]}</p>
+        `
+      })
+    },
+    cancleAddressForm() {
+      this.addressDetails = false
+      this.addressValue1 = ''
+      this.addressValue2 = ''
     }
+    // 
   },
   created() {
     this.getOrdersList()
@@ -216,7 +276,7 @@ export default {
 }
 </script>
 
-<style lang="less" scope>
+<style lang="less">
 .orders {
   .el-breadcrumb {
     margin-bottom: 10px;
@@ -265,6 +325,9 @@ export default {
         font-size: 15px;
       }
     }
+  }
+  .address-react {
+    width: 100%
   }
 }
 </style>
