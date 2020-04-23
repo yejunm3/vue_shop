fdf4ff67e069e135158d4e35b371fc62fac3aff5
叶俊 <2512636001@qq.com>
2020-04-23 14:28:31 +0800

    打包优化

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+// 判断是否是生产模式，配置节点
+const is_remove = []
+if (process.env.NODE_ENV === 'production') {
+  is_remove.push('transform-remove-console')
+}
+
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
-};
+  presets: ['@vue/cli-plugin-babel/preset'],
+  plugins: [...is_remove]
+}
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    devServer: {
-        open: true,
-        port: 8848
-    }
+  publicPath: './', // 表示资源的公共路径
+  devServer: {
+    open: true,
+    port: 8848
+  }
 }
